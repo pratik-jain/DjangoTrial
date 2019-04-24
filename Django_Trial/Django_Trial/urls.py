40221e5bc5aaa5c7ca031cbc7346dee54ccff405
pratik jain <pratikj040@gmail.com>
2019-04-24 19:37:13 +0530

    Create initial Django app

--- Django_Trial/Django_Trial/urls.py
+++ Django_Trial/Django_Trial/urls.py
@@ -3,15 +3,18 @@ Definition of urls for Django_Trial.
 """
 
 from django.conf.urls import include, url
-
+import MyApp1.views
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', Django_Trial.views.home, name='home'),
-    # url(r'^Django_Trial/', include('Django_Trial.Django_Trial.urls')),
+    url(r'^$', MyApp1.views.index, name='index'),
+    url(r'^home$', MyApp1.views.index, name='home'),
+     
+     
+     #url(r'^Django_Trial/', include('Django_Trial.Django_Trial.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
