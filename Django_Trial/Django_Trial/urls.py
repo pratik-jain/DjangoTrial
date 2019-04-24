64f28799c37aa0b7f4322ed265fbd055026f06bf
pratik jain <pratikj040@gmail.com>
2019-04-24 16:09:34 +0530

    Add project files.

---
+++ Django_Trial/Django_Trial/urls.py
@@ -0,0 +1,21 @@
+"""
+Definition of urls for Django_Trial.
+"""
+
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', Django_Trial.views.home, name='home'),
+    # url(r'^Django_Trial/', include('Django_Trial.Django_Trial.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
